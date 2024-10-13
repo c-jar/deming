db973422b92cea2b95f9caa1be4f4dbd5ad961bf
c-jar <clemj1900@gmail.com>
2024-10-13 14:26:02 +0200

    Allow create user

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -15,6 +15,16 @@ use Log;
 class SocialiteController extends Controller
 {
 
+    public const ROLES_MAP = [
+        //'admin' => '1',
+        'user' => '2',
+        'auditee' => '5',
+        'auditor' => '3',
+        //'api' => '4',
+    ];
+
+    public const LOCALES = ['en', 'fr'];
+
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['redirect', 'callback']]);
@@ -48,19 +58,55 @@ class SocialiteController extends Controller
 
         Log::debug("Callback provider : '$provider'");
 
+        // Get additionnal config for current provider
+        $config_name = 'services.socialite_controller.'.$provider;
+        $allow_create_user = false;
+        if(config($config_name)){
+            if(config($config_name.'.allow_create_user')){
+                $allow_create_user = config($config_name.'.allow_create_user');
+            }
+        }
+        Log::debug('Config allow_create_user='.$allow_create_user);
+
+
         try {
-            $socialiteUser = Socialite::with($provider)->user();
+            $socialite_user = Socialite::with($provider)->user();
             $user = null;
 
-            if($socialiteUser->email){
-                $user = User::query()->whereEmail($socialiteUser->email)->first();
+            if($socialite_user->email){
+                $user = User::query()->whereEmail($socialite_user->email)->first();
             } else {
                 Log::warning("User has no attribute email");
             }
      
-            if (!$user) {
-                Log::warning("User [$socialiteUser->id, $socialiteUser->email] not found in deming database");
-                return redirect('login')->withErrors(['socialite' => trans('cruds.login.error.user_not_exist') ]);
+            if (!$user) { 
+                if ($allow_create_user){
+                    $user = new User();
+                    $user->login = $socialite_user->id;
+                    $user->name = $socialite_user->name;
+                    $user->email = $socialite_user->email;
+                    $user->title = "";
+                    $user->role = self::ROLES_MAP['auditee'];
+
+                    $language = self::LOCALES[0];
+                    if ($socialite_user->offsetExists('locale')){
+                        $locale = explode('-', $socialite_user->offsetGet('locale'));
+                        $_language = $locale[0];
+                        if (in_array($_language, self::LOCALES)) $language = $_language;
+                    }
+                    $user->language = $language;
+
+                    // TODO allow null password
+                    $user->password = bin2hex(random_bytes(32));
+
+                    Log::info("Create new user '$user->login' with role '$user->role' from $provider provider");
+                    $user->save();
+
+
+                } else {
+                    Log::warning("User [$socialite_user->id, $socialite_user->email] not found in deming database");
+                    return redirect('login')->withErrors(['socialite' => trans('cruds.login.error.user_not_exist') ]);
+                } 
             }
 
             Log::info("User '$user->login' login with $provider provider");
